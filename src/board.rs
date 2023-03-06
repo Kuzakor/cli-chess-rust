205e54a52910d218bb82892ada57fb1c602fc613
Kuzakor <kuzakor@icloud.com>
2023-03-06 22:20:03 +0100

    Added pieces insert, move and delete

--- src/board.rs
+++ src/board.rs
@@ -1,16 +1,48 @@
 
+use extend::ext;
+
 #[derive(Copy, Clone, Debug)]
 pub struct board {
-   pub layout: [[char;7];7]
-
+   pub layout: [[char;7];7],
 }
 
 impl board {
     pub fn is_check() -> bool {
         todo!()
     }
-    pub fn move_piece(&mut self, x: i32, y:i32, piece: char) {
-        self.layout[x as usize][y as usize] = 'X';
+    pub fn insert_piece(self, x: i32, y:i32, piece: char) -> [[char;7];7]{
+        let mut layout_output = self.layout.clone();
+        layout_output[x as usize][y as usize] = piece;
+        layout_output
+    }
+    pub fn remove_piece(&mut self, x: i32, y:i32) -> [[char;7];7] {
+        let mut layout_output = self.layout.clone();
+        layout_output[x as usize][y as usize] = ' ';
+        layout_output
+    }
+}
+
+#[derive(Copy, Clone, Debug)]
+pub struct piece {   
+    pub piece: char,
+    pub x: i32,
+    pub y: i32,
+}
+
+impl piece {
+    pub fn move_piece(&mut self, x: i32, y: i32, layout:board) -> board{
+        let mut layout_output = layout.clone();
+        layout_output.layout = layout_output.remove_piece(self.x, self.y);
+        layout_output.layout = layout_output.insert_piece(x, y, self.piece);
+        self.x = x;
+        self.y = y;
+        layout_output
+    }
+    pub fn highlight_possible_moves(self) -> () {
+
+        todo!()
+
     }
         
+    
 }
--- src/main.rs
+++ src/main.rs
@@ -2,5 +2,18 @@ mod board;
 
 fn main() {
     let mut a = board::board { layout: [[' ';7];7]};
-    println!("{:#?}", a);
+    let mut horse = board::piece { 
+        piece: 'H',
+        x: 1,
+        y: 1,
+    };
+    nice_print(a);
+}
+
+
+fn nice_print(what: board::board) {
+    for i in 0..7 {
+        println!("{:?}", what.layout[6-i])
+    }
+
 }
