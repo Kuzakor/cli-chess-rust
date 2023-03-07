88cd662b7096ca5808b90531d4f04d52fdad0d82
Kuzakor <kuzakor@icloud.com>
2023-03-07 22:01:56 +0100

    Added so much stuff i cannot count it
    Look at changes

--- src/board.rs
+++ src/board.rs
@@ -1,48 +1,88 @@
-
 use extend::ext;
 
+//New struct - loc
+#[derive(Copy, Clone, Debug)]
+pub struct loc {
+    pub x: i32,
+    pub y: i32,
+}
+//New struct - board
 #[derive(Copy, Clone, Debug)]
 pub struct board {
-   pub layout: [[char;7];7],
+    pub layout: [[char; 7]; 7],
 }
 
 impl board {
     pub fn is_check() -> bool {
         todo!()
     }
-    pub fn insert_piece(self, x: i32, y:i32, piece: char) -> [[char;7];7]{
-        let mut layout_output = self.layout.clone();
-        layout_output[x as usize][y as usize] = piece;
-        layout_output
+    //Function insering a char in desired location
+    pub fn insert_piece(&mut self, loc: loc, piece: char) {
+        self.layout[loc.x as usize][loc.y as usize] = piece;
     }
-    pub fn remove_piece(&mut self, x: i32, y:i32) -> [[char;7];7] {
-        let mut layout_output = self.layout.clone();
-        layout_output[x as usize][y as usize] = ' ';
-        layout_output
+    //Function insering a ' ' in desired location
+    pub fn remove_piece(&mut self, loc: loc) {
+        self.layout[loc.x as usize][loc.y as usize] = ' ';
     }
 }
 
-#[derive(Copy, Clone, Debug)]
-pub struct piece {   
+//New struct - piece
+#[derive(Copy, Clone, Debug)] 
+pub struct piece {
     pub piece: char,
-    pub x: i32,
-    pub y: i32,
+    pub loc: loc,
 }
 
 impl piece {
-    pub fn move_piece(&mut self, x: i32, y: i32, layout:board) -> board{
-        let mut layout_output = layout.clone();
-        layout_output.layout = layout_output.remove_piece(self.x, self.y);
-        layout_output.layout = layout_output.insert_piece(x, y, self.piece);
-        self.x = x;
-        self.y = y;
-        layout_output
+    //Funtion removing current piece and adding a new one in desired location, so basically moving a piece.
+    pub fn move_piece(&mut self, loc: loc, layout: &mut board) {
+        layout.remove_piece(loc {
+            x: self.loc.x,
+            y: self.loc.y,
+        });
+        layout.insert_piece(loc { x: loc.x, y: loc.y }, self.piece);
+        //changing piece loc to new one
+        self.loc.x = loc.x;
+        self.loc.y = loc.y;
     }
-    pub fn highlight_possible_moves(self) -> () {
-
-        todo!()
-
+    //Function getting all posible moves including invalid ones
+    pub fn get_possible_moves(self) -> Vec<loc> {
+        let mut moves: Vec<loc> = Vec::new();
+        //Diffrent move depending on piece
+        match self.piece {
+            'K' => {
+                //Declaring array with values to add to the i instead of doing it manulally:
+                /*moves.push(loc{
+                    x: self.loc.x + 0
+                    y: self.loc.x + 1
+                }
+                etc.
+                )*/
+                let a = [0, 1, 1, 1, 1, 0, 1, -1, 0, -1, -1, -1, -1, 0, -1, 1];
+                for i in (0..a.len()).step_by(2) {
+                    moves.push(loc {
+                        x: self.loc.x + a[i],
+                        y: self.loc.y + a[i + 1],
+                    });
+                }
+            }
+            _ => todo!(),
+        }
+        moves
     }
-        
-    
+    //Funcion creating * in all possible places piece can move
+    pub fn filter_and_highlight_moves(self, moves: Vec<loc>, layout: &mut board) {
+        let mut moves = moves;
+        //Removes invalid moves that are outside the board
+        for i in 0..moves.len(){
+            if moves[i].x < 0 || moves[i].y < 0 || moves[i].x > 7 || moves[i].y > 7{
+                moves.remove(i);
+            }
+        }
+        //Need to add old highlite removal
+        //Inserts * on all posible moves
+        for i in moves {
+            layout.insert_piece(i, '*');
+        }
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,28 @@
 mod board;
-
+use std::io;
 fn main() {
     let mut a = board::board { layout: [[' ';7];7]};
     let mut horse = board::piece { 
-        piece: 'H',
-        x: 1,
-        y: 1,
+        piece: 'K',
+        loc: board::loc {
+            x: 1,
+            y: 1,
+        },
     };
-    nice_print(a);
+    
 }
 
 
 fn nice_print(what: board::board) {
     for i in 0..7 {
-        println!("{:?}", what.layout[6-i])
+        println!("{:?}", what.layout[i])
     }
 
+}
+
+fn get_int() -> i32 {
+    let mut num = String::new();
+    io::stdin().read_line(&mut num).expect("read error");
+    let num: i32 = num.trim().parse().expect("convert error");
+    num
 }
