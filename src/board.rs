bfd8e93f8b90ce7d897a7f91f458c7c63d710dcf
Kuzakor <kuzakor@icloud.com>
2023-03-18 19:14:16 +0100

    Made the code more readable and fixed movement
    No en passant and castle yet

--- src/board.rs
+++ src/board.rs
@@ -1,4 +1,4 @@
-use std::{cmp::Ordering};
+use std::cmp::Ordering;
 
 //New struct - loc
 #[derive(Copy, Clone, Debug, PartialEq)]
@@ -8,9 +8,8 @@ pub struct Loc {
 }
 //New struct - board
 #[derive(Copy, Clone, Debug)]
-pub struct Board{
-    pub layout: [[char; 8]; 8]
-
+pub struct Board {
+    pub layout: [[char; 8]; 8],
 }
 
 impl Board {
@@ -25,13 +24,19 @@ impl Board {
 }
 
 //New struct - piece
-#[derive(Copy, Clone, Debug)] 
+#[derive(Copy, Clone, Debug)]
 pub struct Piece {
     pub piece: char,
     pub loc: Loc,
 }
 
 impl Piece {
+    pub fn new(piece: char, loc: Loc) -> Self {
+        Piece {
+            piece,
+            loc,
+        }
+    }
     //Funtion removing current piece and adding a new one in desired location, so basically moving a piece.
     pub fn move_piece(&mut self, loc: Loc, layout: &mut Board) {
         layout.remove_piece(Loc {
@@ -49,226 +54,187 @@ impl Piece {
         //Diffrent move depending on piece
         match self.piece {
             //King moves
+            //Declaring array with values to add to the index instead of doing it manulally:
+            /*moves.push(loc{
+                x: self.loc.x + 0
+                y: self.loc.x + 1
+            }
+            etc.
+            //Doing this for every piece
+            )*/
             'K' | 'k' => {
-                //Declaring array with values to add to the i instead of doing it manulally:
-                /*moves.push(loc{
-                    x: self.loc.x + 0
-                    y: self.loc.x + 1
-                }
-                etc.
-                )*/
-                let a = [0, 1, 1, 1, 1, 0, 1, -1, 0, -1, -1, -1, -1, 0, -1, 1];
-                for i in (0..a.len()).step_by(2) {
+                let change = [0, 1, 1, 1, 1, 0, 1, -1, 0, -1, -1, -1, -1, 0, -1, 1];
+                for index in (0..change.len()).step_by(2) {
                     moves.push(Loc {
-                        x: self.loc.x + a[i],
-                        y: self.loc.y + a[i + 1],
+                        x: self.loc.x + change[index],
+                        y: self.loc.y + change[index + 1],
                     });
                 }
             }
             //Knight moves
             'N' | 'n' => {
-                let a = [-1,2,1,2,2,1,2,-1,1,-2,-1,-2,-2,-1,-2,1];
-                for i in (0..a.len()).step_by(2) {
+                let change = [-1, 2, 1, 2, 2, 1, 2, -1, 1, -2, -1, -2, -2, -1, -2, 1];
+                for index in (0..change.len()).step_by(2) {
                     moves.push(Loc {
-                        x: self.loc.x + a[i],
-                        y: self.loc.y + a[i + 1],
+                        x: self.loc.x + change[index],
+                        y: self.loc.y + change[index + 1],
                     });
                 }
-            } 
+            }
             //Pawm moves
             'P' => {
-                moves.push(Loc {
-                     x: self.loc.x + 1,
-                     y: self.loc.y,
-                    });
-                moves.push(Loc {
-                    x: self.loc.x + 1,
-                    y: self.loc.y + 1,
-                });
-                moves.push(Loc {
-                    x: self.loc.x + 1,
-                    y: self.loc.y - 1,
-                });
+                let change = [1, 0, 1, 1, 1, -1];
+                for index in (0..change.len()).step_by(2) {
+                    moves.push(Loc {
+                        x: self.loc.x + change[index],
+                        y: self.loc.y + change[index + 1],
+                    })
+                }
                 if self.loc.x == 1 {
                     moves.push(Loc {
                         x: self.loc.x + 2,
                         y: self.loc.y,
-                       });
+                    });
                 }
-                
             }
             //Pawn moves
             'p' => {
-                moves.push(Loc {
-                    x: self.loc.x - 1,
-                    y: self.loc.y,
-                   });
-                moves.push(Loc {
-                    x: self.loc.x - 1,
-                    y: self.loc.x - 1,
-                });
-                moves.push(Loc {
-                    x: self.loc.x - 1,
-                    y: self.loc.x + 1,
-                });
+                let change = [-1, 0, -1, -1, -1, 1];
+                for index in (0..change.len()).step_by(2) {
+                    moves.push(Loc {
+                        x: self.loc.x + change[index],
+                        y: self.loc.y + change[index + 1],
+                    })
+                }
                 if self.loc.x == 5 {
-                   moves.push(Loc {
-                       x: self.loc.x - 2,
-                       y: self.loc.y,
-                      });
-               }
+                    moves.push(Loc {
+                        x: self.loc.x - 2,
+                        y: self.loc.y,
+                    });
+                }
             }
             //Bishop moves
             'B' | 'b' => {
                 for i in 1..8 {
-                    let a = [i, i, -i, -i, i, -i, -i, i];
-                    for b in (0..a.len()).step_by(2){
-                        moves.push(Loc{
-                            x: self.loc.x + a[b],
-                            y: self.loc.y + a[b + 1],
-                    });
+                    let change = [i, i, -i, -i, i, -i, -i, i];
+                    for index in (0..change.len()).step_by(2) {
+                        moves.push(Loc {
+                            x: self.loc.x + change[index],
+                            y: self.loc.y + change[index + 1],
+                        });
                     }
-                    
                 }
             }
             //Rook moves
             'R' | 'r' => {
                 for i in 1..8 {
-                    moves.push(Loc{
-                        x: self.loc.x,
-                        y: self.loc.y + i,
-                    });
-                    moves.push(Loc{
-                        x: self.loc.x + i,
-                        y: self.loc.y,
-                    });
-                    moves.push(Loc{
-                        x: self.loc.x,
-                        y: self.loc.y - i,
-                    });
-                    moves.push(Loc{
-                        x: self.loc.x - i,
-                        y: self.loc.y,
-                    });
+                    let change = [0, i, i, 0, 0, -i, -i, 0];
+                    for index in (0..change.len()).step_by(2) {
+                        moves.push(Loc {
+                            x: self.loc.x + change[index],
+                            y: self.loc.y + change[index + 1],
+                        });
+                    }
                 }
             }
             //Queen Moves
             'Q' | 'q' => {
-            for i in 1..8 {
-                moves.push(Loc{
-                    x: self.loc.x,
-                    y: self.loc.y + i,
-                });
-                moves.push(Loc{
-                    x: self.loc.x + i,
-                    y: self.loc.y,
-                });
-                moves.push(Loc{
-                    x: self.loc.x,
-                    y: self.loc.y - i,
-                });
-                moves.push(Loc{
-                    x: self.loc.x - i,
-                    y: self.loc.y,
-                });
-                moves.push(Loc{
-                    x: self.loc.x + i,
-                    y: self.loc.y + i,
-                });
-                moves.push(Loc{
-                    x: self.loc.x - i,
-                    y: self.loc.y - i,
-                });
-                moves.push(Loc{
-                    x: self.loc.x + i,
-                    y: self.loc.y - i,
-                });
-                moves.push(Loc{
-                    x: self.loc.x - i,
-                    y: self.loc.y + i,
-                });
+                    for i in 1..64 {
+                        let change = [i, i, -i, -i, i, -i, -i, i, 0, i, i, 0, 0, -i, -i, 0];
+                        for index in (0..change.len()).step_by(2) {
+                            moves.push(Loc {
+                                x: self.loc.x + change[index],
+                                y: self.loc.y + change[index + 1],
+                            });
+                        }
+                    }
             }
-        }
             _ => todo!(),
         }
-        println!("{:?}", moves);
         moves
     }
 
-    pub fn filter_moves(self, moves: Vec<Loc>, mut layout: Board) -> Vec<Loc>{
+    pub fn filter_moves(self, moves: Vec<Loc>, layout: Board) -> Vec<Loc> {
         let mut moves = moves;
-        
-        //Removes invalid moves 
-        
-        let mut comeback = 0;
-        let mut block:Vec<i32> = Vec::new();
-        for mut i in 0..moves.len(){
 
-            //Comeback is needed 'cause when removing value, indexes are changing
+        //Removes invalid moves
+
+        let mut comeback = 0;
+        let mut block: Vec<i32> = Vec::new();
+        for mut move_index in 0..moves.len() {
+            //Comeback is needed 'cause when removing value, indexes are changing.
             let mut need_contunue = false;
-            i = i - comeback;
-            
+            move_index -= comeback;
+
             //Moves that are outside the board
 
-            if moves[i].x < 0 || moves[i].y < 0 || moves[i].x > 7 || moves[i].y > 7 {
-                moves.remove(i);
+            if moves[move_index].x < 0
+                || moves[move_index].y < 0
+                || moves[move_index].x > 7
+                || moves[move_index].y > 7
+            {
+                moves.remove(move_index);
                 comeback += 1;
                 continue;
-            } 
+            }
 
-            /*
-            Cheks sign of self.loc - moves[i] and the same for blocking piece.
-            If blocking piece has the same signs as the move for eg. --, 0- ++ etc,
-            it means that the move is "behind" the blocking piece so it removes that move.
-            For more in-depth in exmplanation reach me on reddit : u/Kuzakor.
-             */
+            
+            if self.piece != 'N' && self.piece != 'n' {
+                /*
+                Cheks sign of self.loc - moves[i] and the same for blocking piece.
+                If blocking piece has the same signs as the move for eg. --, 0- ++ etc,
+                it means that the move is "behind" the blocking piece so it removes that move.
+                For more in-depth in exmplanation reach me on reddit : u/Kuzakor.
+                */
+                let x_sign = sign_check(self.loc.x - moves[move_index].x);
+                let y_sign = sign_check(self.loc.y - moves[move_index].y);
 
-            let x_sign = sign_check(self.loc.x - moves[i].x);
-            let y_sign = sign_check(self.loc.y - moves[i].y);
-
-            for block_loc in (0..block.len()).step_by(2){
-                let x_sign_block = sign_check(block[block_loc]);
-                let y_sign_block = sign_check(block[block_loc + 1]);
-                if x_sign == x_sign_block && y_sign == y_sign_block {
-                    moves.remove(i);
-                    comeback += 1;
-                    need_contunue = true;
+                //Checks signs of every piece that may be blockig the move and compares them with signs of the move
+                for block_localization in (0..block.len()).step_by(2) {
+                    let x_sign_block = sign_check(block[block_localization]);
+                    let y_sign_block = sign_check(block[block_localization + 1]);
+                    if x_sign == x_sign_block && y_sign == y_sign_block {
+                        moves.remove(move_index);
+                        comeback += 1;
+                        need_contunue = true;
+                    }
+                }
+                //resets the for loop if move was removed
+                if need_contunue {
+                    continue;
                 }
             }
-            //resets the for loop if move was removed
-            if need_contunue {continue;}
             
 
             //If piece is pawn removes moving by axis if there is no piece
-            let place = layout.layout[moves[i].x as usize][moves[i].y as usize];
+            let place = layout.layout[moves[move_index].x as usize][moves[move_index].y as usize];
 
-            if (self.piece == 'P' || self.piece == 'p') && (moves[i].y != self.loc.y){
+            if (self.piece == 'P' || self.piece == 'p') && (moves[move_index].y != self.loc.y) {
                 if place == ' ' {
-                    moves.remove(i);
+                    moves.remove(move_index);
                     comeback += 1;
                     continue;
                 }
-            }      
+            }
             //Removes possibility to kick own pieces
-            
+
             if place != ' ' {
-                block.push(self.loc.x - moves[i].x);
-                block.push(self.loc.y - moves[i].y);
-                if (place.is_ascii_lowercase() && self.piece.is_ascii_lowercase()) || (place.is_ascii_uppercase() && self.piece.is_ascii_uppercase()){
-                    moves.remove(i);
+                block.push(self.loc.x - moves[move_index].x);
+                block.push(self.loc.y - moves[move_index].y);
+                if (place.is_ascii_lowercase() && self.piece.is_ascii_lowercase())
+                    || (place.is_ascii_uppercase() && self.piece.is_ascii_uppercase())
+                {
+                    moves.remove(move_index);
                     comeback += 1;
                     continue;
                 }
                 continue;
             }
-            
         }
-        
         moves
     }
     //Funcion creating * in all possible places piece can move
     pub fn highlight_moves(self, moves: Vec<Loc>, mut layout: Board) -> Board {
-        
         //Inserts * on all posible moves
         for i in moves {
             layout.insert_piece(i, '*');
@@ -277,18 +243,19 @@ impl Piece {
     }
 }
 
-
+//New type of variable = Sign
 #[derive(PartialEq, Debug)]
-        enum Sign {
-            Plus,
-            Minus,
-            Zero,
-        }
+enum Sign {
+    Plus,
+    Minus,
+    Zero,
+}
 
-fn sign_check(number:i32) -> Sign {
+//Reurns sign of given number (Minus, Zero, One)
+fn sign_check(number: i32) -> Sign {
     match (number).cmp(&0) {
         Ordering::Less => Sign::Minus,
         Ordering::Equal => Sign::Zero,
-        Ordering::Greater => Sign::Plus
-     }
+        Ordering::Greater => Sign::Plus,
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -2,51 +2,65 @@ mod board;
 use std::io;
 fn main() {
     let mut board = board::Board { layout: [[' ';8];8]};
-    let mut queen = board::Piece { 
-        piece: 'P',
-        loc: board::Loc {
-            x: 1,
-            y: 1,
-        },
-    };
-    let mut horse = board::Piece { 
-        piece: 'n',
-        loc: board::Loc {
-            x: 2,
-            y: 0,
-        },
-    };
-    let mut rook = board::Piece { 
-        piece: 'R',
-        loc: board::Loc {
-            x: 2,
-            y: 4,
-        },
-    };
-    board.insert_piece(horse.loc, horse.piece);
-    board.insert_piece(queen.loc, queen.piece);
-    board.insert_piece(rook.loc, rook.piece);
-    loop {
-    board = user_move(board, &mut queen);
-    nice_print(board);
+    let mut uppercase_pieces: Vec<board::Piece> = Vec::new();
+    for i in 0..8{
+        uppercase_pieces.push(board::Piece::new('P', board::Loc { x: 1, y: i }))
     }
-}
+    
+    uppercase_pieces.push(board::Piece::new('R', board::Loc { x: 0, y: 0 }));
+    uppercase_pieces.push(board::Piece::new('R', board::Loc { x: 0, y: 7 }));
+    uppercase_pieces.push(board::Piece::new('N', board::Loc { x: 0, y: 1 }));
+    uppercase_pieces.push(board::Piece::new('N', board::Loc { x: 0, y: 6 }));
+    uppercase_pieces.push(board::Piece::new('B', board::Loc { x: 0, y: 2 }));
+    uppercase_pieces.push(board::Piece::new('B', board::Loc { x: 0, y: 5 }));
+    uppercase_pieces.push(board::Piece::new('Q', board::Loc { x: 0, y: 3 }));
+    uppercase_pieces.push(board::Piece::new('K', board::Loc { x: 0, y: 4 }));
 
-
-fn nice_print(what: board::Board) {
-    println!("    0    1    2    3    4    5    6    7");
-    for i in 0..8 {
-        println!("{} {:?}",i, what.layout[i])
+    for i in uppercase_pieces.iter(){
+        board.insert_piece(i.loc, i.piece);
     }
 
+    loop{
+        nice_print(board);
+        println!("Choose piece: ");
+        let loc = board::Loc {
+            //Here what you normally expect to by x is y because its 2d array not a map nor other stuff
+            x: {
+                println!("y: ");
+                get_int()
+            },
+            y: {
+                println!("x: ");
+                get_int()
+            },
+        };
+        let piece_index = get_piece_from_loc(uppercase_pieces.clone(), loc);
+        match piece_index {
+            Some(x) => {
+                board = user_move(board, &mut uppercase_pieces[x]);
+            }
+            None => {
+                println!("No piece there");
+                continue;
+            }
+        };
+        
+    }
+    //loop {
+    //board = user_move(board, &mut queen);
+    //nice_print(board);
+    //}
 }
 
-fn get_int() -> i32 {
-    let mut num = String::new();
-    io::stdin().read_line(&mut num).expect("read error");
-    let num: i32 = num.trim().parse().expect("convert error");
-    num
+fn get_piece_from_loc(pieces_vec: Vec<board::Piece>, loc:board::Loc) -> Option<usize> {
+    for i in 0..pieces_vec.len(){
+        if pieces_vec[i].loc == loc {
+            return Some(i);
+        }
+    }
+    None
 }
+
 
 fn user_move(mut board: board::Board, piece: &mut board::Piece) -> board::Board {
     println!("You have chosen {} on location {:?}", piece.piece, piece.loc);
@@ -58,6 +72,10 @@ fn user_move(mut board: board::Board, piece: &mut board::Piece) -> board::Board 
     loop {
         let unfiltered_moves = piece.get_possible_moves();
         let possible_moves = piece.filter_moves(unfiltered_moves, board);
+        if possible_moves.len() == 0 {
+            println!("No legal moves possible");
+            break;
+        }
         let loc = board::Loc {
             //Here what you normally expect to by x is y because its 2d array not a map nor other stuff
             x: {
@@ -77,6 +95,22 @@ fn user_move(mut board: board::Board, piece: &mut board::Piece) -> board::Board 
             continue;
       }
     }
+    board
 
 }
 
+//Funtion prinint given Board in human-readable format
+fn nice_print(what: board::Board) {
+    println!("    0    1    2    3    4    5    6    7");
+    for i in 0..8 {
+        println!("{} {:?}",i, what.layout[i])
+    }
+
+}
+//Funtion getting i32 input from user
+fn get_int() -> i32 {
+    let mut num = String::new();
+    io::stdin().read_line(&mut num).expect("read error");
+    let num: i32 = num.trim().parse().expect("convert error");
+    num
+}
